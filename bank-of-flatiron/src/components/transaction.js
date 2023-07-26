ada50f57d5ba04f1b6fcbfbf9655c13302846095
maimuna003 <135684307+maimuna003@users.noreply.github.com>
2023-07-26 22:27:00 +0300

    Update transaction.js

--- bank-of-flatiron/src/components/transaction.js
+++ bank-of-flatiron/src/components/transaction.js
@@ -1,1 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import Filter from './filter';
+import Form from './form';
 
+function Trans() {
+  const [transactions, setTransactions] = useState([]);
+  
+
+  useEffect(() => {
+    fetch("https://api.npoint.io/b40674980bba1f5ee977") 
+      .then((res) => res.json())
+      .then((data) => {
+        setTransactions(data.transactions);
+        
+      });
+  }, []);
+
+  const handleNewTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const handleDeleteTransaction = (id) => {
+    const newTransactions = transactions.filter((transaction) => transaction.id !== id);
+    setTransactions(newTransactions);
+  };
+
+  return (
+    <div className="transactions-container">
+      <Form onSubmit={handleNewTransaction} />
+      <Filter transactions={transactions} onDelete={handleDeleteTransaction} />
+    </div>
+  );
+}
+
+export default Trans;
